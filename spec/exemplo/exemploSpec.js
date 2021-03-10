9555756655d1ee0020c16553fc79d24ddbe01109
JessicaPrior <jsscprior@gmail.com>
2021-03-10 10:37:12 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,10 @@
+describe("Suite de teste de exemplo com nodeJs", function () {
+
+    it("Deve demonstrar o uso do jasmine com nodeJs", function () {
+        expect(true).toEqual(true)
+    })
+
+    it("Deve demonstrar o uso do karma em modo de escuta", function () {
+        expect(1).toEqual(1)
+    })
+})
